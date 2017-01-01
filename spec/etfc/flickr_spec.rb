c9e89cd68af48243db16e8ecfcf589ecf142f91d
Maximilian Haack <mxhaack@gmail.com>
2017-01-01 22:11:34 +0200

    Use VCR to record Flickr API interactions
    Arguably using VCR for two specs is overkill however manually mocking
    the flickr authentication flow is error prone and annoying.

--- spec/etfc/flickr_spec.rb
+++ spec/etfc/flickr_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 describe ETFC::Flickr do
   context '.search' do
-    it { expect(ETFC::Flickr.search('match')).to match(/http/) }
-    it { expect(ETFC::Flickr.search('13371717171717171634517293')).to be_nil }
+    it 'returns URL for match' do
+      VCR.use_cassette('flickr_search_match') do
+        expect(ETFC::Flickr.search('match')).to match(/http/)
+      end
+    end
+
+    it 'returns nil for no match' do
+      VCR.use_cassette('flickr_search_no_match') do
+        expect(ETFC::Flickr.search('13371717171717171634517293')).to be_nil
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,6 @@ require 'webmock/rspec'
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
